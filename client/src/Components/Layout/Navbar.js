dbbd1715cce05fa43a30a43ab09d30059e8958d7
haseena-hassan <haseena2199@gmail.com>
2020-07-15 20:30:00 +0530

    home nav

--- client/src/Components/Layout/Navbar.js
+++ client/src/Components/Layout/Navbar.js
@@ -6,7 +6,7 @@ export default class Topnav extends Component {
         return (
             <div>
                 <Navbar  bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Auth-application</Navbar.Brand>
+                    <Navbar.Brand href="/">Auth-application</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
