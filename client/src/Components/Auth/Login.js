c063cbb0d945df15a20e0c14002466f5dfe75612
haseena-hassan <haseena2199@gmail.com>
2020-07-15 20:55:31 +0530

    form edits

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -4,20 +4,52 @@ import Button from 'react-bootstrap/Button'
 import '../../Assets/Css/Layout/layout.css'
 
 export default class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+          email: "",
+          password: "",
+          errors: {}
+        }
+      }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(userData)
+    }
+
     render() {
+        const { errors } = this.state;
+        
         return (
             <div className='center'>
                 <h1>Login</h1>
-                <Form>
+                <Form noValidate onSubmit={this.onSubmit}>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
+                        <Form.Control 
+                            type="email" 
+                            onChange={this.onChange}
+                            value={this.state.email}
+                            error={errors.email}
+                            id="email"
+                            placeholder="Enter email"/>
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control 
+                            type="password"  
+                            onChange={this.onChange}
+                            value={this.state.password}
+                            error={errors.password}
+                            id="password"
+                            placeholder="Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
--- client/src/Components/Auth/Signup.js
+++ client/src/Components/Auth/Signup.js
@@ -4,25 +4,74 @@ import Button from 'react-bootstrap/Button'
 import '../../Assets/Css/Layout/layout.css'
 
 export default class Register extends Component {
+    constructor() {
+        super()
+        this.state = {
+          email: "",
+          phone: "",
+          password: "",
+          password2: "",
+          errors: {}
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const newUser = {
+            email: this.state.email,
+            phone: this.state.phone,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+        console.log(newUser);
+    }
+
     render() {
+        const { errors } = this.state;
+        
         return (
             <div className='center'>
                 <h1>SignUp</h1>
-                <Form>
+                <Form noValidate onSubmit={this.onSubmit}>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        
-                        </Form.Text>
+                        <Form.Control 
+                            type="email" 
+                            onChange={this.onChange}
+                            value={this.state.email}
+                            error={errors.email}
+                            id="email"
+                            placeholder="Enter email"/>
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="text" placeholder="Phone Number" />
+                        <Form.Control 
+                            type="text"  
+                            onChange={this.onChange}
+                            value={this.state.phone}
+                            error={errors.phone}
+                            id="phone"
+                            placeholder="Phone Number" />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label></Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control 
+                            type="password"  
+                            onChange={this.onChange}
+                            value={this.state.password}
+                            error={errors.password}
+                            id="password"
+                            placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control 
+                            type="password"  
+                            onChange={this.onChange}
+                            value={this.state.password2}
+                            error={errors.password2}
+                            id="password2"
+                            placeholder="Confirm Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
