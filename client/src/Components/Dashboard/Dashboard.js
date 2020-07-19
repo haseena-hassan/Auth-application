1f1340f1ced91937950da6d9268eede2f6fb8ddf
haseena-hassan <haseena2199@gmail.com>
2020-07-20 00:21:04 +0530

    added dashboard

---
+++ client/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { logoutUser } from "../../actions/authActions"
+
+class Dashboard extends Component {
+
+    onLogoutClick = e => {
+        e.preventDefault()
+        this.props.logoutUser()
+    }
+
+    render() {
+        const { user } = this.props.auth
+        return (
+            <div className='center'>
+                <h1>Welcome {user.name.split(" ")[0]}</h1>
+                <Button onClick={this.onLogoutClick} variant="primary" >Logout</Button>
+            </div>
+        )
+    }
+}
+
+
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard)
